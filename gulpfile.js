c4e880fcf8b2e9dfb28a8d9d908c41a9960de066
Izmar <Slonikkot1309>
2021-07-06 02:21:07 +0300

    begin

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
+
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
     fileinclude = require('gulp-file-include'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
-    return gulp.src("./src/assets/scss/**/*.scss")
+    return gulp.src("./src/**/*.scss")
         .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('fileinclude', function() {
-    return gulp.src('./src/pages/index.html')
-        .pipe(fileinclude({
+    return gulp.src([
+        './src/pages/about_us/about_us.html',
+        './src/pages/homepage/index.html'
+    ]).pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
@@ -24,8 +27,8 @@ gulp.task("watch", function(done) {
         server:'.'
     })
 
-    gulp.watch("./src/assets/scss/**/*.scss", gulp.series('sass'));
-    gulp.watch("./src/pages/*.html").on('change', gulp.series('fileinclude'));
+    gulp.watch("./src/**/*.scss", gulp.series('sass'));
+    gulp.watch("./src/pages/**/*.html").on('change', gulp.series('fileinclude'));
     gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
