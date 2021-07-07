bf3af9af730ffd8ddb90c51243a865cdbdb4a2e6
a.vykhodchenko <Slonikkot1309>
2021-07-07 10:17:01 +0300

    first

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
     fileinclude = require('gulp-file-include'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
-    return gulp.src("./src/**/*.scss")
+    return gulp.src("./src/assets/scss/*.scss")
         .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
@@ -19,16 +18,25 @@ gulp.task('fileinclude', function() {
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy',function(){
+    return gulp.src('./src/assets/icons/**/*.{svg,png}')
+        .pipe(gulp.dest('./dist/icons'))
 });
 
 gulp.task("watch", function(done) {
     browserSync.init({
-        server:'.'
+        server: {
+            baseDir: "./dist",
+            index: "index.html"
+        }
     })
 
     gulp.watch("./src/**/*.scss", gulp.series('sass'));
     gulp.watch("./src/pages/**/*.html").on('change', gulp.series('fileinclude'));
+    gulp.watch("./src/**/*.{svg,png}").on('change', gulp.series('copy'));
     gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
