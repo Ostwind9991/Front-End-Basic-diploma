6547708620f30b3c601942774129d06d4704da9b
Izmar <Slonikkot1309>
2021-07-26 00:42:10 +0300

    mobil finish it

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,29 @@
-$('a').click(function() {
-    $('.action').css('display', 'block');
-});
+function toggleMeTu(a, currEle) {
+    var t = currEle;
+    var e = document.getElementById(a);
+    if (!e) return true;
+    if (e.style.display == "none") {
+        t.setAttribute('aria-expanded', 'true');
+        e.style.display = "block"
+    }
+    else {
+        t.setAttribute('aria-expanded', 'false');
+        e.style.display = "none"
+    }
+    return true;
+}
+
+function toggleMe(a, currEle) {
+    var t = currEle;
+    var e = document.getElementById(a);
+    if (!e) return true;
+    if (e.style.display == "block") {
+        t.setAttribute('aria-expanded', 'true');
+        e.style.display = "none"
+    }
+    else {
+        t.setAttribute('aria-expanded', 'false');
+        e.style.display = "none"
+    }
+    return true;
+}
